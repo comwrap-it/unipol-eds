384348f90ee6d406f84e3c79305d669f005f4f14
r.guarneri <r.guarneri@reply.it>
2025-10-21 11:54:30 +0200

    Updated teaser

--- blocks/teaser/teaser.js
+++ blocks/teaser/teaser.js
@@ -1,1 +1,27 @@
-console.log('Teaser block initialized');
+import { createOptimizedPicture } from '../../scripts/aem.js';
+import { moveInstrumentation } from '../../scripts/scripts.js';
+
+export default function decorate(block) {
+  const [image, imageAlt, text] = block.children[0].children;
+
+  const teaser = document.createElement('div');
+  teaser.classList.add('teaser-wrapper');
+
+  if (image) {
+    const img = image.querySelector('img');
+    const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]);
+    moveInstrumentation(img, optimizedPic.querySelector('img'));
+    img.closest('picture').replaceWith(optimizedPic);
+    if (img) teaser.append(img);
+  }
+
+  const content = document.createElement('div');
+  content.classList.add('teaser-content');
+
+  if (imageAlt) content.append(imageAlt);
+  if (text) content.append(text);
+
+  teaser.append(content);
+  block.textContent = '';
+  block.append(teaser);
+}
