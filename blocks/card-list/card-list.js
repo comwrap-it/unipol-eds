41248cff496da85f92fb8c3337142c3f17fd10c0
c.giunta <c.giunta@reply.it>
2025-11-03 16:33:24 +0100

    Support Universal Editor card components in card-list
    Updated card-list block to handle both traditional card data and pre-decorated card components from the Universal Editor. Cards are now limited to a maximum of 4, prioritizing Universal Editor components before traditional data. Updated JSON schema to define card-list children constraints.

--- blocks/card-list/card-list.js
+++ blocks/card-list/card-list.js
@@ -38,11 +38,34 @@ async function createCard(cardData, index) {
 export default async function decorate(block) {
   if (!block) return;
 
-  const cards = [...block.children];
+  // Get all child elements (both traditional and Universal Editor children)
+  const allChildren = [...block.children];
+
+  // Separate traditional card data from Universal Editor card components
+  const cardData = [];
+  const cardComponents = [];
+
+  allChildren.forEach((child) => {
+    if (child.classList.contains('card')) {
+      // This is already a decorated card component from Universal Editor
+      cardComponents.push(child);
+    } else {
+      // This is traditional card data that needs to be processed
+      cardData.push(child);
+    }
+  });
 
   // Limit to maximum 4 cards as per specification
-  if (cards.length > 4) {
-    cards.splice(4);
+  const maxCards = 4;
+  let cardsToProcess = [];
+
+  // Add Universal Editor card components first
+  cardsToProcess = [...cardComponents];
+
+  // Add traditional card data up to the limit
+  const remainingSlots = maxCards - cardComponents.length;
+  if (remainingSlots > 0) {
+    cardsToProcess = [...cardsToProcess, ...cardData.slice(0, remainingSlots)];
   }
 
   // Create grid container
@@ -50,11 +73,20 @@ export default async function decorate(block) {
   container.className = 'cards-container';
 
   // Set grid columns based on number of cards
-  const numCards = cards.length;
+  const numCards = cardsToProcess.length;
   container.dataset.cardCount = numCards;
 
   // Process each card
-  const cardPromises = cards.map(async (card, index) => {
+  const cardPromises = cardsToProcess.map(async (card, index) => {
+    if (card.classList.contains('card')) {
+      // Already a decorated card component, just wrap it
+      const cardElement = document.createElement('div');
+      cardElement.className = 'card-item';
+      cardElement.dataset.index = index;
+      cardElement.appendChild(card);
+      return cardElement;
+    }
+    // Traditional card data that needs decoration
     const cardElement = await createCard(card, index);
     moveInstrumentation(card, cardElement);
     return cardElement;
