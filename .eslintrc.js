63b7b2c69ff58de724f3d65ba7ba98c119b9e769
nimai_kraus <n.kraus@goreply.io>
2025-11-14 18:31:58 +0100

    correct fields name to avoid collapsible fields error

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,6 @@ module.exports = {
     'linebreak-style': ['error', 'unix'], // enforce unix linebreaks
     'no-param-reassign': [2, { props: false }], // allow modifying properties of param
     'xwalk/max-cells': ['warn', { '*': 60 }],
-    'xwalk/no-orphan-collapsible-fields': 'off',
+    'xwalk/no-orphan-collapsible-fields': 'error',
   },
 };
