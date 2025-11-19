374658657ce9608320a50a912629901458ec6231
HRMLPT-134\Mauro.Spinosi <mauro.spinosi@hrm.group>
2025-11-19 16:08:10 +0100

    fixed js error

--- blocks/footer-list-link/footer-list-link.js
+++ blocks/footer-list-link/footer-list-link.js
@@ -4,34 +4,30 @@ export default function decorate(block) {
 
   [...block.children].forEach((row) => {
     const linkDiv = document.createElement('div');
-    const children = [...row.children];
 
-    const hideTitleText = children[0]?.textContent.trim();
-    const hideTitle = hideTitleText === 'true';
+    const children = [...row.children];
+    const hideTitle = children[0]?.textContent.trim() === 'true';
     const title = children[1]?.textContent.trim();
     const linkText = children[2]?.textContent.trim();
-    const linkHref = children[3]?.querySelector('a')?.getAttribute('href') || '#';
+    const linkHref = children[3]?.querySelector('a')?.getAttribute('href');
 
     if (hideTitle && title) {
       const titleDiv = document.createElement('div');
-      titleDiv.className = 'footer-link-title';
       const p = document.createElement('p');
       p.textContent = title;
       titleDiv.append(p);
       linkDiv.append(titleDiv);
     }
 
-    if (linkText && !(hideTitle && linkText.toLowerCase() === 'true') && !(hideTitleText === 'false' && linkText.toLowerCase() === 'false')) {
-      const linkContainerDiv = document.createElement('div');
-      linkContainerDiv.className = 'button-container';
-      const a = document.createElement('a');
-      a.href = linkHref;
-      a.title = linkHref;
-      a.className = 'button';
-      a.textContent = linkText;
-      linkContainerDiv.append(a);
-      linkDiv.append(linkContainerDiv);
-    }
+    const linkContainerDiv = document.createElement('div');
+    linkContainerDiv.className = 'button-container';
+    const a = document.createElement('a');
+    a.href = linkHref || '#';
+    a.title = linkHref || '';
+    a.className = 'button';
+    a.textContent = linkText || 'Link';
+    linkContainerDiv.append(a);
+    linkDiv.append(linkContainerDiv);
 
     wrapper.append(linkDiv);
   });
