3df1d6614aa40fdb9b9340d0279d83b7d698789a
HRMLPT-134\Mauro.Spinosi <mauro.spinosi@hrm.group>
2025-11-19 16:27:36 +0100

    added style to link-list-section

--- blocks/footer-list-link/footer-list-link.js
+++ blocks/footer-list-link/footer-list-link.js
@@ -4,30 +4,25 @@ export default function decorate(block) {
 
     const hideTitleText = children[0]?.textContent.trim();
     const hideTitle = hideTitleText === 'true';
-    const titleDiv = children[1]; // div del titolo
+    const titleDiv = children[1];
     const linkTextP = children[2]?.querySelector('p');
     const linkHref = children[3]?.querySelector('a');
 
-    // Nascondi il <p> con true/false
     if (children[0]) children[0].textContent = '';
 
-    // Gestisci titolo
     if (titleDiv) {
       titleDiv.style.display = hideTitle ? 'block' : 'none';
     }
 
-    // Gestisci link
     if (linkTextP && linkHref) {
       const linkText = linkTextP.textContent.trim();
 
-      // Determina se mostrare il link
       const showLink = !((hideTitle && linkText.toLowerCase() === 'true') ||
                          (!hideTitle && linkText.toLowerCase() === 'false'));
 
       linkHref.style.display = showLink ? 'block' : 'none';
       if (showLink) linkHref.textContent = linkText;
 
-      // Nascondi completamente il <p> originale
       linkTextP.style.display = 'none';
       linkTextP.textContent = '';
     }
