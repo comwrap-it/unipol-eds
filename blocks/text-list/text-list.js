6f35fe82cc3cc7d06a08809d7cd886054f58b173
HRMLPT-134\Mauro.Spinosi <mauro.spinosi@hrm.group>
2025-11-20 12:12:37 +0100

    added light / dark select in text list

--- blocks/text-list/text-list.js
+++ blocks/text-list/text-list.js
@@ -1,16 +1,36 @@
 export default function decorate(block) {
+  const wrapper = block.closest('.text-list-wrapper');
+  const lastRow = block.children[block.children.length - 1];
+  const styleValue = lastRow?.children?.[4]?.textContent?.trim?.() || '';
+
+  if (wrapper) {
+    const allLinks = wrapper.querySelectorAll('a');
+    const allTitles = wrapper.querySelectorAll('h1, h2, h3, h4, h5, h6');
+
+    if (styleValue === 'text-list-dark') {
+      allLinks.forEach(a => a.classList.add('text-list-link-dark'));
+      allTitles.forEach(t => t.classList.add('text-list-title-dark'));
+    }
+
+    if (styleValue === 'text-list-light') {
+      allLinks.forEach(a => a.classList.add('text-list-link-light'));
+      allTitles.forEach(t => t.classList.add('text-list-title-light-dark'));
+    }
+  }
+
   [...block.children].forEach((row) => {
     const children = [...row.children];
 
     const hideTitle = children[0]?.textContent.trim() === 'true';
-    const titleDiv = children[1];
+
+    const titleElem = children[1]?.querySelector('h1, h2, h3, h4, h5, h6');
     const linkTextP = children[2]?.querySelector('p');
     const linkHref = children[3]?.querySelector('a');
 
     if (children[0]) children[0].textContent = '';
 
-    if (titleDiv) {
-      titleDiv.style.display = hideTitle ? 'block' : 'none';
+    if (titleElem) {
+      titleElem.style.display = hideTitle ? 'block' : 'none';
     }
 
     if (linkTextP && linkHref) {
