69f24877e2387f531d5ab56f235d30bde7d1d37d
HRMLPT-134\Mauro.Spinosi <mauro.spinosi@hrm.group>
2025-11-25 13:56:33 +0100

    fix: decorate unipol-footer

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,5 +1,10 @@
 import { getMetadata } from '../../scripts/aem.js';
 import { loadFragment } from '../fragment/fragment.js';
+import decorateTextLink from '../text-list/text-list.js';
+import decorateFooterDownload from '../footer-download-section/footer-download-section.js';
+import decorateFooterPrivacy from '../footer-privacy-section/footer-privacy-section.js';
+import decorateFooterSocial from '../footer-social-section/footer-social-section.js';
+import decorateFooterCopyright from '../footer-copyright-section/footer-copyright-section.js';
 
 /**
  * loads and decorates the footer
@@ -13,8 +18,30 @@ export default async function decorate(block) {
 
   // decorate footer DOM
   block.textContent = '';
-  const footer = document.createElement('div');
-  while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
+  const footerContainer = document.createElement('div');
+  footerContainer.className = 'unipol-footer';
 
-  block.append(footer);
+  while (fragment.firstElementChild) footerContainer.append(fragment.firstElementChild);
+
+  const textListBlocks = footerContainer.querySelectorAll('.text-list');
+  if (textListBlocks) decorateTextLink(textListBlocks);
+
+  // --- FOOTER DOWNLOAD SECTION ---
+  const downloadBlock = footerContainer.querySelector('.footer-download-section');
+  if (downloadBlock) decorateFooterDownload(downloadBlock);
+
+  // --- FOOTER PRIVACY SECTION ---
+  const privacyBlock = footerContainer.querySelector('.footer-privacy-section');
+  if (privacyBlock) decorateFooterPrivacy(privacyBlock);
+
+  // --- FOOTER SOCIAL SECTION ---
+  const socialBlock = footerContainer.querySelector('.footer-social-section');
+  if (socialBlock) decorateFooterSocial(socialBlock);
+
+  // --- FOOTER COPYRIGHT SECTION ---
+  const copyrightBlock = footerContainer.querySelector('.footer-copyright-section');
+  if (copyrightBlock) decorateFooterCopyright(copyrightBlock);
+
+  // append final footerContainer to block
+  block.append(footerContainer);
 }
