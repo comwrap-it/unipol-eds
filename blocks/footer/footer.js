08a0f8fbcc2586f98fe1d2d2232a3aaa37797b98
HRMLPT-134\Mauro.Spinosi <mauro.spinosi@hrm.group>
2025-11-25 21:01:40 +0100

    general css refactor

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,10 +1,5 @@
 import { getMetadata } from '../../scripts/aem.js';
 import { loadFragment } from '../fragment/fragment.js';
-import decorateTextLink from '../text-list/text-list.js';
-import decorateFooterDownload from '../footer-download-section/footer-download-section.js';
-import decorateFooterPrivacy from '../footer-privacy-section/footer-privacy-section.js';
-import decorateFooterSocial from '../footer-social-section/footer-social-section.js';
-import decorateFooterCopyright from '../footer-copyright-section/footer-copyright-section.js';
 
 /**
  * loads and decorates the footer
@@ -26,10 +21,6 @@ export default async function decorate(block) {
   // append all fragment children to footerContainer temporarily
   while (fragment.firstElementChild) footerContainer.append(fragment.firstElementChild);
 
-  // decorate text-link blocks
-  const textListBlocks = footerContainer.querySelectorAll('.text-list');
-  if (textListBlocks) decorateTextLink(textListBlocks);
-
   // --- CREATE TOP AND BOTTOM CONTAINERS ---
   const topContainer = document.createElement('div');
   topContainer.className = 'unipol-footer-top';
@@ -43,28 +34,21 @@ export default async function decorate(block) {
 
   const downloadWrapper = footerContainer.querySelector('.footer-download-section-wrapper');
   if (downloadWrapper) {
-    decorateFooterDownload(downloadWrapper.querySelector('.footer-download-section'));
     topContainer.append(downloadWrapper);
   }
 
   const privacyWrapper = footerContainer.querySelector('.footer-privacy-section-wrapper');
   if (privacyWrapper) {
-    decorateFooterPrivacy(privacyWrapper.querySelector('.footer-privacy-section'));
     bottomContainer.append(privacyWrapper);
   }
 
   const socialWrapper = footerContainer.querySelector('.footer-social-section-wrapper');
   if (socialWrapper) {
-    decorateFooterSocial(socialWrapper.querySelector('.footer-social-section'));
     bottomContainer.append(socialWrapper);
   }
 
   // --- COPYRIGHT SECTION ---
   const copyrightBlock = footerContainer.querySelector('.footer-copyright-section');
-  if (copyrightBlock) {
-    decorateFooterCopyright(copyrightBlock);
-  }
-
   // clear footerContainer and append organized sections
   footerContainer.textContent = '';
   footerContainer.append(topContainer, bottomContainer);
