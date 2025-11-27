3a84dcd3241edff3576da61833d68c55b5c4c99b
Rosario Guarneri <r.guarneri@reply.it>
2025-11-27 13:34:42 +0100

    feat: rework on swiper import in insurance-product-carousel-widget

--- blocks/insurance-product-carousel-widget/insurance-product-carousel-widget.js
+++ blocks/insurance-product-carousel-widget/insurance-product-carousel-widget.js
@@ -1,10 +1,11 @@
 /* eslint-disable import/extensions */
 /* eslint-disable no-console */
 // eslint-disable-next-line import/no-unresolved
-import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.mjs';
+import loadSwiper from '../../scripts/delayed.js';
 
-export default async function decorate() {
+export default async function handleInsuranceProductCarouselWidget() {
   let isStylesLoaded = false;
+
   async function ensureStylesLoaded() {
     if (isStylesLoaded) return;
     const { loadCSS } = await import('../../scripts/aem.js');
@@ -16,80 +17,103 @@ export default async function decorate() {
 
   await ensureStylesLoaded();
 
-  function reorderExpandingDots(orderCallback) {
-    const expandingDots = document.querySelector('.scroll-indicator .expanding-dots');
-    if (!expandingDots) return;
+  // Initialize Swiper
+  loadSwiper().then((Swiper) => {
+    try {
+      const carousels = document.querySelectorAll('.insurance-product-carousel-wrapper');
+      carousels.forEach((carousel) => {
+        const swiperEl = carousel.querySelector('.swiper');
 
-    const rectangle = expandingDots.querySelector('.rectangle')?.cloneNode(true);
-    const svgs = [...expandingDots.querySelectorAll('svg')].map((svg) => svg.cloneNode(true));
+        if (swiperEl.dataset.initialized) return;
 
-    expandingDots.textContent = '';
+        swiperEl.dataset.initialized = 'true';
 
-    const orderedNodes = orderCallback({ rectangle, svgs });
+        // eslint-disable-next-line no-unused-vars
+        function insuranceProductCarousel({ swiper, extendParams, on }) {
+          extendParams({
+            debugger: false,
+          });
 
-    orderedNodes.forEach((node) => {
-      if (node) expandingDots.appendChild(node);
-    });
-  }
+          let endReached = false;
+          let startReached = false;
 
-  // eslint-disable-next-line no-unused-vars
-  function insuranceProductCarousel({ swiper, extendParams, on }) {
-    extendParams({
-      debugger: false,
-    });
+          function reorderExpandingDots(orderCallback) {
+            const expandingDots = carousel.querySelector('.scroll-indicator .expanding-dots');
+            if (!expandingDots) return;
 
-    let endReached = false;
-    let startReached = false;
+            const rectangle = expandingDots.querySelector('.rectangle')?.cloneNode(true);
+            const svgs = [...expandingDots.querySelectorAll('svg')].map((svg) => svg.cloneNode(true));
 
-    on('init', () => {
-      document.querySelectorAll('.swiper-navigation-icon').forEach((el) => { el.remove(); });
-    });
-    on('slideChange', () => {
-      if (endReached || startReached) return;
-      endReached = false;
-      startReached = false;
-      reorderExpandingDots(({ rectangle, svgs }) => {
-        const [ellipseOne, ellipseTwo] = svgs;
-        return [ellipseOne, rectangle, ellipseTwo];
+            expandingDots.textContent = '';
+
+            const orderedNodes = orderCallback({ rectangle, svgs });
+
+            orderedNodes.forEach((node) => {
+              if (node) expandingDots.appendChild(node);
+            });
+          }
+
+          on('init', () => {
+            carousel.querySelectorAll('.swiper-navigation-icon').forEach((el) => {
+              el.remove();
+            });
+            carousel.querySelectorAll('.swiper-notification').forEach((el) => {
+              el.remove();
+            });
+          });
+          on('slideChange', () => {
+            if (endReached || startReached) return;
+            endReached = false;
+            startReached = false;
+            reorderExpandingDots(({ rectangle, svgs }) => {
+              const [ellipseOne, ellipseTwo] = svgs;
+              return [ellipseOne, rectangle, ellipseTwo];
+            });
+          });
+          on('fromEdge', () => {
+            endReached = false;
+            startReached = false;
+          });
+          on('reachBeginning', () => {
+            startReached = true;
+            reorderExpandingDots(({ rectangle, svgs }) => [rectangle, ...svgs]);
+          });
+          on('reachEnd', () => {
+            endReached = true;
+            reorderExpandingDots(({ rectangle, svgs }) => [...svgs, rectangle]);
+          });
+        }
+
+        // Init Swiper
+        // eslint-disable-next-line no-unused-vars
+        const swiper = new Swiper(swiperEl, {
+          // Install Plugin To Swiper
+          modules: [insuranceProductCarousel],
+          navigation: {
+            nextEl: carousel.querySelector('.swiper-button-next'),
+            prevEl: carousel.querySelector('.swiper-button-prev'),
+          },
+          speed: 700,
+          slidesPerView: 'auto',
+          slidesPerGroup: 1,
+          breakpoints: {
+            // when window width is >= 768px
+            768: {
+              slidesPerView: 'auto',
+              allowTouchMove: false,
+            },
+          },
+          resistanceRatio: 0.85,
+          touchReleaseOnEdges: true,
+          effect: 'slide',
+          // Enable debugger
+          debugger: true,
+        });
       });
-    });
-    on('fromEdge', () => {
-      endReached = false;
-      startReached = false;
-    });
-    on('reachBeginning', () => {
-      startReached = true;
-      reorderExpandingDots(({ rectangle, svgs }) => [rectangle, ...svgs]);
-    });
-    on('reachEnd', () => {
-      endReached = true;
-      reorderExpandingDots(({ rectangle, svgs }) => [...svgs, rectangle]);
-    });
-  }
-
-  // Init Swiper
-  // eslint-disable-next-line no-unused-vars
-  const swiper = new Swiper('.swiper', {
-    // Install Plugin To Swiper
-    modules: [insuranceProductCarousel],
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    speed: 700,
-    slidesPerView: 3,
-    slidesPerGroup: 1,
-    breakpoints: {
-      // when window width is >= 768px
-      768: {
-        slidesPerView: 4,
-        allowTouchMove: false,
-      },
-    },
-    resistanceRatio: 0.85,
-    touchReleaseOnEdges: true,
-    effect: 'slide',
-    // Enable debugger
-    debugger: true,
+    } catch {
+      // Error creating Swiper instance
+    }
+  }).catch(() => {
+    // Error loading Swiper from CDN
   });
 }
