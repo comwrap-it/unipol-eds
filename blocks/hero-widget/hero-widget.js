31c79f04617e166ff5f2c4bbf9a9350528f6b546
lorpisana <lorenzo.pisana@1nnovation.net>
2025-12-03 14:45:34 +0100

    feat: add editorial dark theme property to widgets

---
+++ blocks/hero-widget/hero-widget.js
@@ -0,0 +1,16 @@
+// Adds theme-dark class when editorial property is set to true
+
+export default function decorateHeroWidget(block) {
+  const rows = [...block.children];
+
+  const darkThemeText = rows[1]?.textContent?.trim().toLowerCase();
+  const darkTheme = darkThemeText === 'true';
+
+  const section = block.closest('.section');
+
+  if (darkTheme) {
+    section?.classList.add('theme-dark');
+  } else {
+    section?.classList.remove('theme-dark');
+  }
+}
