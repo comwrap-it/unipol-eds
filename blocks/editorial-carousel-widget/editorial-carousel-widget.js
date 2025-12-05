a73f1c35a0320f8c7ab8541e912a60b7d3fc8cde
DaveRPY <sa.scarano@reply.it>
2025-12-05 17:02:19 +0100

    feat: editorial carousel widget prototype

--- blocks/editorial-carousel-widget/editorial-carousel-widget.js
+++ blocks/editorial-carousel-widget/editorial-carousel-widget.js
@@ -1,134 +1,141 @@
-// import loadSwiper from '../../scripts/lib/utils.js';
+import loadSwiper from '../../scripts/lib/utils.js';
+
+let isStylesLoaded = false;
+
+async function ensureStylesLoaded() {
+  if (isStylesLoaded) return;
+  const { loadCSS } = await import('../../scripts/aem.js');
+  await Promise.all([
+    loadCSS(
+      `${window.hlx.codeBasePath}/blocks/editorial-carousel-widget/editorial-carousel-widget.css`,
+    ),
+  ]);
+  isStylesLoaded = true;
+}
 
 export default async function handleEditorialProductCarouselWidget() {
-  const block = document.querySelector('.editorial-product-carousel-container');
-  // eslint-disable-next-line no-console
-  console.log('editorial carousel widget', block);
+  const block = document.querySelector('.editorial-carousel-container');
 
-  // const rows = [...block.children];
-  // const darkThemeText = rows[1]?.textContent?.trim().toLowerCase();
-  // const darkThemeValue = darkThemeText === 'true';
+  const rows = [...block.children];
+  const darkThemeText = rows[1]?.textContent?.trim().toLowerCase();
+  const darkThemeValue = darkThemeText === 'true';
 
-  // const section = block.closest('.section');
-  // if (darkThemeValue) {
-  //   section?.classList.add('theme-dark');
-  // } else {
-  //   section?.classList.remove('theme-dark');
-  // }
+  const section = block.closest('.section');
+  if (darkThemeValue) {
+    section?.classList.add('theme-dark');
+  } else {
+    section?.classList.remove('theme-dark');
+  }
 
-  // let isStylesLoaded = false;
+  await ensureStylesLoaded();
 
-  // async function ensureStylesLoaded() {
-  //   if (isStylesLoaded) return;
-  //   const { loadCSS } = await import('../../scripts/aem.js');
-  //   await Promise.all([
-  // eslint-disable-next-line max-len
-  //     loadCSS(`${window.hlx.codeBasePath}/blocks/insurance-product-carousel-widget/insurance-product-carousel-widget.css`),
-  //   ]);
-  //   isStylesLoaded = true;
-  // }
+  // Initialize Swiper
+  loadSwiper()
+    .then((Swiper) => {
+      try {
+        const carousels = document.querySelectorAll(
+          '.editorial-carousel-wrapper',
+        );
+        carousels.forEach((carousel) => {
+          const swiperEl = carousel.querySelector('.swiper');
 
-  // await ensureStylesLoaded();
+          if (swiperEl.dataset.initialized) return;
 
-  // // Initialize Swiper
-  // loadSwiper().then((Swiper) => {
-  //   try {
-  //     const carousels = document.querySelectorAll('.insurance-product-carousel-wrapper');
-  //     carousels.forEach((carousel) => {
-  //       const swiperEl = carousel.querySelector('.swiper');
+          swiperEl.dataset.initialized = 'true';
 
-  //       if (swiperEl.dataset.initialized) return;
+          // eslint-disable-next-line no-unused-vars
+          function insuranceProductCarousel({ swiper, extendParams, on }) {
+            extendParams({
+              debugger: false,
+            });
 
-  //       swiperEl.dataset.initialized = 'true';
+            let endReached = false;
+            let startReached = false;
 
-  //       // eslint-disable-next-line no-unused-vars
-  //       function insuranceProductCarousel({ swiper, extendParams, on }) {
-  //         extendParams({
-  //           debugger: false,
-  //         });
+            function setExpandedDot({ isBeginning, isEnd }) {
+              const expandingDots = carousel.querySelector('.expanding-dots');
+              const dots = expandingDots.querySelectorAll('span');
+              // Clear previous state
+              dots.forEach((dot) => dot.classList.remove('expanded'));
+              // Decide which to expand
+              if (isBeginning) {
+                dots[0]?.classList.add('expanded');
+              } else if (isEnd) {
+                dots[2]?.classList.add('expanded');
+              } else {
+                dots[1]?.classList.add('expanded');
+              }
+            }
 
-  //         let endReached = false;
-  //         let startReached = false;
+            on('init', () => {
+              carousel
+                .querySelectorAll('.swiper-navigation-icon')
+                .forEach((el) => {
+                  el.remove();
+                });
+              carousel
+                .querySelectorAll('.swiper-notification')
+                .forEach((el) => {
+                  el.remove();
+                });
+            });
+            on('slideChange', () => {
+              if (endReached || startReached) return;
+              endReached = false;
+              startReached = false;
+              setExpandedDot({ isBeginning: false, isEnd: false });
+            });
+            on('fromEdge', () => {
+              endReached = false;
+              startReached = false;
+            });
+            on('reachBeginning', () => {
+              startReached = true;
+              setExpandedDot({ isBeginning: true, isEnd: false });
+            });
+            on('reachEnd', () => {
+              endReached = true;
+              setExpandedDot({ isBeginning: false, isEnd: true });
+            });
+          }
 
-  //         function setExpandedDot({ isBeginning, isEnd }) {
-  //           const expandingDots = carousel.querySelector('.expanding-dots');
-  //           const dots = expandingDots.querySelectorAll('span');
-  //           // Clear previous state
-  //           dots.forEach((dot) => dot.classList.remove('expanded'));
-  //           // Decide which to expand
-  //           if (isBeginning) {
-  //             dots[0]?.classList.add('expanded');
-  //           } else if (isEnd) {
-  //             dots[2]?.classList.add('expanded');
-  //           } else {
-  //             dots[1]?.classList.add('expanded');
-  //           }
-  //         }
-
-  //         on('init', () => {
-  //           carousel.querySelectorAll('.swiper-navigation-icon').forEach((el) => {
-  //             el.remove();
-  //           });
-  //           carousel.querySelectorAll('.swiper-notification').forEach((el) => {
-  //             el.remove();
-  //           });
-  //         });
-  //         on('slideChange', () => {
-  //           if (endReached || startReached) return;
-  //           endReached = false;
-  //           startReached = false;
-  //           setExpandedDot({ isBeginning: false, isEnd: false });
-  //         });
-  //         on('fromEdge', () => {
-  //           endReached = false;
-  //           startReached = false;
-  //         });
-  //         on('reachBeginning', () => {
-  //           startReached = true;
-  //           setExpandedDot({ isBeginning: true, isEnd: false });
-  //         });
-  //         on('reachEnd', () => {
-  //           endReached = true;
-  //           setExpandedDot({ isBeginning: false, isEnd: true });
-  //         });
-  //       }
-
-  //       // Init Swiper
-  //       // eslint-disable-next-line no-unused-vars
-  //       const swiper = new Swiper(swiperEl, {
-  //         // Install Plugin To Swiper
-  //         modules: [insuranceProductCarousel],
-  //         a11y: false,
-  //         navigation: {
-  //           nextEl: carousel.querySelector('.swiper-button-next'),
-  //           prevEl: carousel.querySelector('.swiper-button-prev'),
-  //         },
-  //         speed: 700,
-  //         slidesPerView: 3,
-  //         slidesPerGroup: 1,
-  //         breakpoints: {
-  //           // width >= 1200
-  //           1200: {
-  //             slidesPerView: 4,
-  //             allowTouch: false,
-  //           },
-  //           // when window width is >= 768px
-  //           768: {
-  //             slidesPerView: 3,
-  //             allowTouchMove: false,
-  //           },
-  //         },
-  //         resistanceRatio: 0.85,
-  //         touchReleaseOnEdges: true,
-  //         effect: 'slide',
-  //         // Enable debugger
-  //         debugger: true,
-  //       });
-  //     });
-  //   } catch {
-  //     // Error creating Swiper instance
-  //   }
-  // }).catch(() => {
-  //   // Error loading Swiper from CDN
-  // });
+          // Init Swiper
+          // eslint-disable-next-line no-unused-vars
+          const swiper = new Swiper(swiperEl, {
+            // Install Plugin To Swiper
+            modules: [insuranceProductCarousel],
+            a11y: false,
+            navigation: {
+              nextEl: carousel.querySelector('.swiper-button-next'),
+              prevEl: carousel.querySelector('.swiper-button-prev'),
+            },
+            speed: 700,
+            slidesPerView: 3,
+            slidesPerGroup: 1,
+            breakpoints: {
+              // width >= 1200
+              1200: {
+                slidesPerView: 4,
+                allowTouch: false,
+              },
+              // when window width is >= 768px
+              768: {
+                slidesPerView: 3,
+                allowTouchMove: false,
+              },
+            },
+            resistanceRatio: 0.85,
+            touchReleaseOnEdges: true,
+            effect: 'slide',
+            // Enable debugger
+            debugger: true,
+          });
+        });
+      } catch {
+        // Error creating Swiper instance
+      }
+    })
+    .catch(() => {
+      // Error loading Swiper from CDN
+    });
 }
