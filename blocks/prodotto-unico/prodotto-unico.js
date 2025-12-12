9ea176f0d377f86cf2f2b91c0eb17bf65a4ff6f5
c.giunta <c.giunta@reply.it>
2025-12-12 12:59:16 +0100

    Added missing file

--- blocks/prodotto-unico/prodotto-unico.js
+++ blocks/prodotto-unico/prodotto-unico.js
@@ -12,12 +12,15 @@ export default async function decorate(block) {
     console.log('CSS caricato per componente Prodotto Unico');
 
     // Carica i file JavaScript in ordine (tutti type="module")
-    // L'ordine è importante: runtime -> polyfills -> main
+    // L'ordine è importante: runtime -> polyfills -> vendor -> main
     await loadScript('/static/js/runtime.js', { type: 'module' });
     console.log('runtime.js caricato');
 
     await loadScript('/static/js/polyfills.js', { type: 'module' });
     console.log('polyfills.js caricato');
+
+    await loadScript('/static/js/vendor.js', { type: 'module' });
+    console.log('vendor.js caricato');
 
     await loadScript('/static/js/main.js', { type: 'module' });
     console.log('main.js caricato - Componente Angular Prodotto Unico pronto');
