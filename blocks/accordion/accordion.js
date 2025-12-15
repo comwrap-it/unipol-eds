2124cac53beaec08a5465a6eb7e63d77321672e5
Rosario Guarneri <r.guarneri@reply.it>
2025-12-15 18:36:32 +0100

    fix: accordion.js description multi lines richtext

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -28,7 +28,7 @@ export function createAccordion(accordionLabel, accordionDescription) {
 
   const content = document.createElement('div');
   content.className = 'accordion-content';
-  content.append(...accordionDescription?.value);
+  content.append(...(accordionDescription?.value ?? []));
   if (isAuthorMode(content)) {
     wrapper.classList.add('open');
   }
