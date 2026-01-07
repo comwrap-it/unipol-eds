0ce2117e9de1aa18e6a99a7d8a704baffc907ac3
Rosario Guarneri <r.guarneri@reply.it>
2026-01-07 12:40:55 +0100

    refactored category-card.js

--- blocks/category-card/category-card.js
+++ blocks/category-card/category-card.js
@@ -10,7 +10,7 @@
 import { create3Dicons } from '@unipol-ds/components/atoms/icons-3D/icons-3D.js';
 import { createCategoryChip } from '@unipol-ds/components/atoms/category-chip/category-chip.js';
 import { createOptimizedPicture } from '../../scripts/aem.js';
-import { createTextElementFromRow } from '../../scripts/domHelpers.js';
+import { createTextElementFromObj } from '../../scripts/domHelpers.js';
 import { extractInstrumentationAttributes, restoreInstrumentation } from '../../scripts/utils.js';
 
 let isStylesLoaded = false;
@@ -26,27 +26,23 @@ async function ensureStylesLoaded() {
 
 /**
  *
- * @param image
- * @param altText
+ * @param image { { src, instrumentation, altText } }
  * @return {HTMLDivElement} cardImage
  */
-const createImageCard = (image, altText) => {
+const createImageCard = (image) => {
   const cardImage = document.createElement('div');
   cardImage.className = 'category-card-image';
 
   if (image) {
-    const img = image.querySelector('img');
-    const src = img?.getAttribute('src') ?? '';
     const optimizedPic = createOptimizedPicture(
-      src,
-      altText?.textContent?.trim() || '',
+      image.src,
+      image.altText,
       false,
       [{ media: '(max-width: 768)', width: '343' }, { media: '(max-width: 1200)', width: '302' }, { media: '(max-width: 1440)', width: '373' }, { media: '(min-width: 1441)', width: '426' }],
     );
 
-    const imgInstrumentation = extractInstrumentationAttributes(img);
-    if (imgInstrumentation) {
-      restoreInstrumentation(optimizedPic, imgInstrumentation);
+    if (image.instrumentation) {
+      restoreInstrumentation(optimizedPic, image.instrumentation);
     }
 
     cardImage.appendChild(optimizedPic);
@@ -55,32 +51,41 @@ const createImageCard = (image, altText) => {
   return cardImage;
 };
 
+/**
+ *
+ * @param category {string}
+ * @param categoryChips {{icon, text, textInstrumentation}}
+ * @return {HTMLDivElement}
+ */
 const createCategoryChips = (category, categoryChips) => {
   const chips = document.createElement('div');
   chips.className = 'category-chips';
 
   categoryChips.forEach((chip) => {
-    const icon = chip.children[0]?.textContent?.trim();
-    const text = chip.children[1]?.textContent?.trim();
-    let textInstrumentation;
-    if (text) {
-      textInstrumentation = extractInstrumentationAttributes(chip.children[1]);
-    }
-
-    if (icon !== undefined && text !== undefined) {
-      chips.appendChild(createCategoryChip(category, icon, text, textInstrumentation));
+    if (chip.icon !== undefined && chip.text !== undefined) {
+      // eslint-disable-next-line max-len
+      chips.appendChild(createCategoryChip(category, chip.icon, chip.text, chip.textInstrumentation));
     }
   });
 
   return chips;
 };
 
+/**
+ *
+ * @param title {{value, instrumentation}}
+ * @param subTitle {{value, instrumentation}}
+ * @param note {{value, instrumentation}}
+ * @param category {string}
+ * @param categoryChips {{icon, text, textInstrumentation}[]}
+ * @return {HTMLDivElement} Card Content Element
+ */
 const createCardContent = (title, subTitle, note, category, categoryChips) => {
   const cardContent = document.createElement('div');
   cardContent.className = 'category-card-inner-content';
 
-  const titleElement = createTextElementFromRow(title, 'title', 'h3');
-  const subTitleElement = createTextElementFromRow(subTitle, 'subtitle', 'p');
+  const titleElement = createTextElementFromObj(title, 'title', 'h3');
+  const subTitleElement = createTextElementFromObj(subTitle, 'subtitle', 'p');
 
   const textElement = document.createElement('div');
   textElement.className = 'category-card-text';
@@ -88,7 +93,7 @@ const createCardContent = (title, subTitle, note, category, categoryChips) => {
   textElement.appendChild(titleElement);
   textElement.appendChild(subTitleElement);
 
-  const noteElement = createTextElementFromRow(note, 'note', 'p');
+  const noteElement = createTextElementFromObj(note, 'note', 'p');
 
   const categoryChipsElement = createCategoryChips(category, categoryChips);
 
@@ -98,11 +103,119 @@ const createCardContent = (title, subTitle, note, category, categoryChips) => {
   return cardContent;
 };
 
+/**
+ *
+ * @param category {string}
+ * @return {*}
+ */
 const create3DcategoryIcons = (category) => {
   const vehiclesShowIcon = category === 'mobility';
   const homeShowIcon = category === 'property';
   const personalShowIcon = category === 'welfare';
   return create3Dicons(vehiclesShowIcon, homeShowIcon, personalShowIcon);
+};
+
+// eslint-disable-next-line max-len
+/**
+ *   Category Card Data Rows
+ *
+ *   rows[0] --> title
+ *   rows[1] --> subtitle
+ *   rows[2] --> note
+ *   rows[3] --> image
+ *   rows[4] --> imageSR
+ *   rows[5] --> category
+ *   rows[6] --> firstCategoryChip icon and text
+ *   rows[7] --> secondCategoryChip icon and text
+ *   rows[8] --> thirdCategoryChip icon and text
+ *   rows[9] --> fourthCategoryChip icon and text
+ *
+ * @param rows
+ * @return {
+ *          {
+ *            title: {value, instrumentation},
+ *            description: {value, instrumentation},
+ *            note: {value, instrumentation},
+ *            image: {src, instrumentation, altText},
+ *            category: {string},
+ *            categoryChips: {icon, text, textInstrumentation}[]
+ *          }
+ *         }
+ */
+const extractValuesFromRows = (rows) => {
+  const title = {};
+  title.value = rows[0]?.textContent?.trim() || '';
+  title.instrumentation = extractInstrumentationAttributes(rows[0]);
+
+  const description = {};
+  description.value = rows[1]?.textContent?.trim() || '';
+  description.instrumentation = extractInstrumentationAttributes(rows[1]);
+
+  const note = {};
+  note.value = rows[2]?.textContent?.trim() || '';
+  note.instrumentation = extractInstrumentationAttributes(rows[2]);
+
+  const image = {};
+  const img = rows[3]?.querySelector('img');
+  image.src = img?.getAttribute('src') ?? '';
+  image.instrumentation = extractInstrumentationAttributes(rows[3]);
+  image.altText = rows[4]?.textContent?.trim() || '';
+
+  const category = rows[5]?.textContent?.trim() || '';
+
+  const categoryChips = [];
+  rows.slice(6).forEach((chip) => {
+    const categoryChip = {};
+    categoryChip.icon = chip.children[0]?.textContent?.trim();
+    categoryChip.text = chip.children[1]?.textContent?.trim();
+
+    if (categoryChip.text) {
+      categoryChip.textInstrumentation = extractInstrumentationAttributes(chip.children[1]);
+    }
+
+    categoryChips.push(categoryChip);
+  });
+
+  return {
+    title,
+    description,
+    note,
+    image,
+    category,
+    categoryChips,
+  };
+};
+
+/**
+ *
+ * @param title {{ value, instrumentation }}
+ * @param description {{value, instrumentation}}
+ * @param note {{value, instrumentation}}
+ * @param image {{src, instrumentation, altText}}
+ * @param category {string}
+ * @param categoryChips {{icon, text, textInstrumentation}[]}
+ * @return {HTMLDivElement} Category Card Element
+ */
+const createCategoryCard = (title, description, note, image, category, categoryChips) => {
+  // Create card structure
+  const card = document.createElement('div');
+  card.className = 'category-card-content';
+
+  const imageEl = createImageCard(image);
+  card.appendChild(imageEl);
+
+  const cardContent = createCardContent(title, description, note, category, categoryChips);
+  card.appendChild(cardContent);
+
+  const iconsElement = create3DcategoryIcons(category);
+  card.appendChild(iconsElement);
+
+  // Append card content
+  if (cardContent.children.length > 0) {
+    card.appendChild(cardContent);
+  }
+
+  return card;
 };
 
 /**
@@ -121,41 +234,16 @@ export default async function decorateCategoryCard(block) {
     return;
   }
 
-  // Create card structure
-  const card = document.createElement('div');
-  card.className = 'category-card-content';
-
-  /*
-  Category Card Data Rows
-
-  rows[0] --> title
-  rows[1] --> subtitle
-  rows[2] --> note
-  rows[3] --> image
-  rows[4] --> imageSR
-  rows[5] --> category
-  rows[6] --> firstCategoryChip icon and text
-  rows[7] --> secondCategoryChip icon and text
-  rows[8] --> thirdCategoryChip icon and text
-  rows[9] --> fourthCategoryChip icon and text
-
-   */
   const rows = Array.from(block.children);
+  const {
+    title,
+    description,
+    note,
+    image, category,
+    categoryChips,
+  } = extractValuesFromRows(rows);
 
-  const image = createImageCard(rows[3], rows[4]);
-  card.appendChild(image);
-
-  const category = rows[5]?.textContent?.trim() || '';
-  const cardContent = createCardContent(rows[0], rows[1], rows[2], category, rows.slice(6));
-  card.appendChild(cardContent);
-
-  const iconsElement = create3DcategoryIcons(category);
-  card.appendChild(iconsElement);
-
-  // Append card content
-  if (cardContent.children.length > 0) {
-    card.appendChild(cardContent);
-  }
+  const card = createCategoryCard(title, description, note, image, category, categoryChips);
 
   // Preserve blockName if present (needed for loadBlock)
   if (block.dataset.blockName) {
