13af0a99521b5f62e7a0eb0a0fbeabc39dcb1631
nimai_kraus <n.kraus@goreply.io>
2026-01-08 15:44:46 +0100

    Refactor dynamic gallery widget by removing unused files and ensuring styles are loaded for dynamic gallery rows

--- blocks/dynamic-gallery-row/dynamic-gallery-row.js
+++ blocks/dynamic-gallery-row/dynamic-gallery-row.js
@@ -1,13 +1,24 @@
 import { createDynamicGalleryCardFromRows } from '../dynamic-gallery-card/dynamic-gallery-card.js';
 
+let isStylesLoaded = false;
+async function ensureStylesLoaded() {
+  if (isStylesLoaded) return;
+  const { loadCSS } = await import('../../scripts/aem.js');
+  await Promise.all([
+    loadCSS(`${window.hlx.codeBasePath}/blocks/atoms/buttons/standard-button/standard-button.css`),
+    loadCSS(`${window.hlx.codeBasePath}/blocks/atoms/tag/tag.css`),
+    loadCSS(`${window.hlx.codeBasePath}/blocks/dynamic-gallery-card/dynamic-gallery-card.css`),
+  ]);
+  isStylesLoaded = true;
+}
+
 export default async function decorate(block) {
   if (!block) return;
+  await ensureStylesLoaded();
 
   const rows = Array.from(block.children);
   const galleryRow = document.createElement('div');
   galleryRow.className = 'dynamic-gallery-row';
-
-  // if (rows.length === 0) return;
 
   rows.forEach(async (row) => {
     const childrenRows = Array.from(row.children);
--- blocks/dynamic-gallery/dynamic-gallery.js
+++
@@ -1,8 +0,0 @@
-export default async function decorate(block) {
-  if (!block) return;
-
-  const gallery = document.createElement('div');
-  gallery.className = 'dynamic-gallery';
-
-  block.replaceChildren(gallery);
-}
