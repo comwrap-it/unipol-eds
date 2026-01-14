647ebaf66ad4985b7613d25ebea179ad28d0151e
Rosario Guarneri <r.guarneri@reply.it>
2026-01-14 10:30:51 +0100

    fixed accessibility and download when hidden in accordion content in accordion-download block

--- blocks/accordion-download/accordion-download.js
+++ blocks/accordion-download/accordion-download.js
@@ -2,6 +2,35 @@ import { createTextElementFromObj } from '@unipol-ds/scripts/domHelpers.js';
 import { extractInstrumentationAttributes, restoreInstrumentation } from '@unipol-ds/scripts/utils.js';
 import { createDownloadTile } from '../download-tile-molecule/download-tile-molecule.js';
 
+const closeAccordion = (icon, content) => {
+  icon.classList.add('un-icon-plus');
+  icon.classList.remove('un-icon-minus');
+  content.querySelectorAll('.download-tile-molecule').forEach((tile) => {
+    const icons = tile.querySelectorAll('.download-icon');
+    icons.forEach((icon) => {
+      icon.setAttribute('aria-hidden', 'true');
+      icon.setAttribute('tabindex', '-1');
+    });
+  });
+  content.setAttribute('aria-hidden', 'true');
+  content.style.maxHeight = '0';
+};
+
+const openAccordion = (icon, content) => {
+  icon.classList.remove('un-icon-plus');
+  icon.classList.add('un-icon-minus');
+  content.querySelectorAll('.download-tile-molecule').forEach((tile) => {
+      const icons = tile.querySelectorAll('.download-icon');
+      icons.forEach((icon) => {
+        icon.setAttribute('aria-hidden', 'false');
+        icon.setAttribute('tabindex', '0');
+      });
+  });
+
+  content.setAttribute('aria-hidden', 'false');
+  content.style.maxHeight = `${content.scrollHeight + 20}px`;
+};
+
 export async function createAccordionDownload(accordionLabel, downloadTiles) {
   const wrapper = document.createElement('div');
   wrapper.className = 'accordion';
@@ -18,9 +47,12 @@ export async function createAccordionDownload(accordionLabel, downloadTiles) {
 
   const content = document.createElement('div');
   content.className = 'accordion-content';
+  content.setAttribute('aria-hidden', 'true');
+
   while (downloadTiles.length > 0) {
     const downloadTile = downloadTiles.shift();
     const downloadTileEl = createDownloadTile(downloadTile);
+    downloadTileEl.querySelector('.download-icon').setAttribute('tabindex', '-1');
     restoreInstrumentation(downloadTileEl, downloadTile.instrumentation);
     content.appendChild(downloadTileEl);
   }
@@ -31,13 +63,9 @@ export async function createAccordionDownload(accordionLabel, downloadTiles) {
     const isOpen = wrapper.classList.toggle('open');
 
     if (isOpen) {
-      icon.classList.remove('un-icon-plus');
-      icon.classList.add('un-icon-minus');
-      content.style.maxHeight = `${content.scrollHeight + 20}px`;
+      openAccordion(icon, content);
     } else {
-      icon.classList.add('un-icon-plus');
-      icon.classList.remove('un-icon-minus');
-      content.style.maxHeight = '0';
+      closeAccordion(icon, content);
     }
   });
 
@@ -48,7 +76,7 @@ const extractValuesFromRows = (rows) => {
   const config = {};
 
   config.value = rows[0]?.textContent?.trim() || '';
-  config.instrumentation = extractInstrumentationAttributes(rows[0]);
+  config.instrumentation = extractInstrumentationAttributes(rows[0].querySelector('p'));
 
   config.downloadTiles = [];
 
