6752648a0fdd507c22e3e9c66ebf43b170310708
Rosario Guarneri <r.guarneri@reply.it>
2026-01-16 09:18:03 +0100

    accordion-download rendered like direct download when only one download-tile is configured

--- blocks/accordion-download/accordion-download.js
+++ blocks/accordion-download/accordion-download.js
@@ -1,8 +1,10 @@
 import { createTextElementFromObj } from '@unipol-ds/scripts/domHelpers.js';
 import { extractInstrumentationAttributes } from '@unipol-ds/scripts/utils.js';
+import { createIconButton } from '@unipol-ds/components/atoms/buttons/icon-button/icon-button.js';
+import { BUTTON_ICON_SIZES, BUTTON_VARIANTS } from '@unipol-ds/constants/index.js';
 import { createDownloadTile } from '../download-tile-molecule/download-tile-molecule.js';
 
-const closeAccordion = (icon, content) => {
+const closeDownloadAccordion = (icon, content) => {
   icon.classList.add('un-icon-plus');
   icon.classList.remove('un-icon-minus');
   content.querySelectorAll('.download-tile-molecule').forEach((tile) => {
@@ -16,7 +18,7 @@ const closeAccordion = (icon, content) => {
   content.style.maxHeight = '0';
 };
 
-const openAccordion = (icon, content) => {
+const openDownloadAccordion = (icon, content) => {
   icon.classList.remove('un-icon-plus');
   icon.classList.add('un-icon-minus');
   content.querySelectorAll('.download-tile-molecule').forEach((tile) => {
@@ -38,36 +40,54 @@ export async function createAccordionDownload(accordionLabel, downloadTiles) {
   const header = document.createElement('div');
   header.className = 'accordion-header';
 
-  const labelEl = createTextElementFromObj(accordionLabel, 'accordion-label', 'span');
+  if (downloadTiles && downloadTiles.length > 1) {
+    const labelEl = createTextElementFromObj(accordionLabel, 'accordion-label', 'span');
 
-  const icon = document.createElement('span');
-  icon.className = 'accordion-icon un-icon-plus';
+    const icon = document.createElement('span');
+    icon.className = 'accordion-icon un-icon-plus';
 
-  header.append(labelEl, icon);
+    header.append(labelEl, icon);
 
-  const content = document.createElement('div');
-  content.className = 'accordion-content';
-  content.setAttribute('aria-hidden', 'true');
+    const content = document.createElement('div');
+    content.className = 'accordion-content';
+    content.setAttribute('aria-hidden', 'true');
 
-  while (downloadTiles.length > 0) {
-    const downloadTile = downloadTiles.shift();
-    const downloadTileEl = createDownloadTile(downloadTile);
-    downloadTileEl.querySelector('.download-icon').setAttribute('tabindex', '-1');
-    // restoreInstrumentation(downloadTileEl, downloadTile.instrumentation);
-    content.appendChild(downloadTileEl);
-  }
-
-  wrapper.append(header, content);
-
-  header.addEventListener('click', () => {
-    const isOpen = wrapper.classList.toggle('open');
-
-    if (isOpen) {
-      openAccordion(icon, content);
-    } else {
-      closeAccordion(icon, content);
+    while (downloadTiles.length > 0) {
+      const downloadTile = downloadTiles.shift();
+      const downloadTileEl = createDownloadTile(downloadTile);
+      downloadTileEl.querySelector('.download-icon').setAttribute('tabindex', '-1');
+      content.appendChild(downloadTileEl);
     }
-  });
+
+    wrapper.append(header, content);
+
+    header.addEventListener('click', () => {
+      const isOpen = wrapper.classList.toggle('open');
+
+      if (isOpen) {
+        openDownloadAccordion(icon, content);
+      } else {
+        closeDownloadAccordion(icon, content);
+      }
+    });
+  } else {
+    header.classList.add('direct-download');
+    const labelEl = createTextElementFromObj(downloadTiles[0], 'accordion-label', 'span');
+
+    const downloadIcon = createIconButton(
+      'un-icon-download-simple',
+      BUTTON_VARIANTS.SECONDARY,
+      BUTTON_ICON_SIZES.LARGE,
+      downloadTiles[0].href,
+      true,
+    );
+    downloadIcon.classList.add('download-icon');
+    downloadIcon.setAttribute('aria-label', `Apri il file ${downloadTiles[0].value} in una nuova finestra`);
+
+    header.append(labelEl, downloadIcon);
+
+    wrapper.append(header);
+  }
 
   return wrapper;
 }
